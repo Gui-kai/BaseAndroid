1f1add1a4c1596487b83a9a6269fedb1421294dd
guikai <916126728@qq.com>
2018-12-30 22:39:10 +0800

    modify build.gradle

--- DelegationAdapter-dev_java/sample/src/main/java/com/kevin/delegationadapter/sample/home/HomeActivity.java
+++ DelegationAdapter-dev_java/sample/src/main/java/com/kevin/delegationadapter/sample/home/HomeActivity.java
@@ -29,9 +29,9 @@ import java.util.List;
  * HomeActivity
  *
  * @author zwenkai@foxmail.com, Created on 2018-04-27 16:12:00
- *         Major Function：<b></b>
- *         <p/>
- *         注:如果您修改了本类请填写以下内容作为记录，如非本人操作劳烦通知，谢谢！！！
+ * Major Function：<b></b>
+ * <p/>
+ * 注:如果您修改了本类请填写以下内容作为记录，如非本人操作劳烦通知，谢谢！！！
  * @author mender，Modified Date Modify Content:
  */
 
