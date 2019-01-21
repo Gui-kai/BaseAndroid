002f38bbd76c117c0382b4dc38b8e18dd3762758
Gui-kai <916126728@qq.com>
2019-01-21 16:08:58 +0800

    updata HelloWorld

--- TestApplication/app/src/main/java/com/guikai/test/HelloWorld/HelloWorldActivity.java
+++ TestApplication/app/src/main/java/com/guikai/test/HelloWorld/HelloWorldActivity.java
@@ -1,7 +1,9 @@
-package com.guikai.test;
+package com.guikai.test.HelloWorld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.guikai.test.R;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
