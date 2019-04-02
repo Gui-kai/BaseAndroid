d40047a7c555dd83461c339c90979ad81e4be370
Gui-kai <916126728@qq.com>
2019-04-02 20:14:24 +0800

    login page style

---
+++ TestApplication/app/src/main/java/com/guikai/test/StyleView/LoginStyle.java
@@ -0,0 +1,15 @@
+package com.guikai.test.StyleView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.guikai.test.R;
+
+public class LoginStyle extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
