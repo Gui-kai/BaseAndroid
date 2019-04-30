750177f2210a5b0b9b8827fc84bf182d47a771ef
Gui-kai <916126728@qq.com>
2019-04-30 16:29:54 +0800

    悬浮窗的实现

---
+++ TestApplication/app/src/main/java/com/guikai/test/floatwindow/FloatWindow.java
@@ -0,0 +1,84 @@
+package com.guikai.test.floatwindow;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.guikai.test.R;
+
+public class FloatWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_float);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(FloatWindow.this, FloatingButtonService.class));
+            }
+        } else if (requestCode == 1) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(FloatWindow.this, FloatingImageDisplayService.class));
+            }
+        } else if (requestCode == 2) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(FloatWindow.this, FloatingVideoService.class));
+            }
+        }
+    }
+
+    public void startFloatingButtonService(View view) {
+        if (FloatingButtonService.isStarted) {
+            return;
+        }
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
+            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0);
+        } else {
+            Toast.makeText(this, "已打开悬浮", Toast.LENGTH_SHORT);
+            startService(new Intent(FloatWindow.this, FloatingButtonService.class));
+        }
+    }
+
+    public void startFloatingImageDisplayService(View view) {
+        if (FloatingImageDisplayService.isStarted) {
+            return;
+        }
+        if (!Settings.canDrawOverlays(this)){
+            Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
+            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 1);
+        } else {
+            startService(new Intent(FloatWindow.this,FloatingImageDisplayService.class));
+        }
+    }
+
+    public void startFloatingVideoService(View view) {
+        if (FloatingVideoService.isStarted) {
+            return;
+        }
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
+            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 2);
+        } else {
+            startService(new Intent(FloatWindow.this,FloatingVideoService.class));
+        }
+    }
+}
